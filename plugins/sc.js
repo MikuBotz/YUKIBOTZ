d28d74476d87e1087a0ab504146154cb7d85a251
Johannes <88168966+MikuBotz@users.noreply.github.com>
2021-10-04 02:47:43 +0700

    Update sc.js

--- plugins/sc.js
+++ plugins/sc.js
@@ -1,71 +1,9 @@
-let fs = require ('fs')
-let handler  = async (m, { conn, usedPrefix: _p }) => {
-	const ftoko = { key : { fromMe: false, participant : `0@s.whatsapp.net` },message: {orderMessage: { message: `tes`,thumbnail: fs.readFileSync("./src/logo.jpg")}}}
-const freply = {key:{ fromMe:false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: `status@broadcast` } : {}) }, message: {
-
-
-
-					"productMessage": {
-
-						"product": {
-
-							"productImage": {
-
-								"url": "https://mmg.whatsapp.net/d/f/Am1sSqpVypFpsQawFUFkm4HgkPRqEx8rt32niyBmL4Wa.enc",
-
-								"mimetype": "image/jpeg",
-
-								"fileSha256": "KbJC20DoVEdDw+8WF1EqwtPsdPUTF8/xQbhg+65P3q4=",
-
-								"fileLength": "43344",
-
-								"height": 1080,
-
-								"width": 1080,
-
-								"mediaKey": "cX+6c20dws6B++0slmMNXcCk7omK+zvheoN6087j9nl=",
-
-								"fileEncSha256": "BGO1C/OttoScb1UxDrGlwsI9eImocg1zwbLgYKmecXs=",
-
-								"directPath": "/v/t62.7118-24/20036572_1210576852672540_4032358369544328852_n.enc?oh=d0e477e1bf0a01bfcf328776ab50ccee&oe=6043238E",
-
-								"mediaKeyTimestamp": "1612168223",
-
-								"jpegThumbnail": global.thumbnail ? global.thumbnail : Buffer.alloc(0)
-
-		},
-
-							"productId": "3872465552870232",
-
-							"title": `Rp 999.999.999.999`,
-
-							"description" : `INI KAK SC NYA YUKIBOT`,
-
-	"productImageCount": 1
-
-						},
-
-						"businessOwnerJid": "994400448210@s.whatsapp.net"}}}    
-
-let info = `
-sc: https://github.com/MikuBotz/YUKIBOTZ
-`.trim()
-await conn.sendButton(m.chat, info, 'Made By Johannes ♥️', '↪️ BACK TO MENU', '#menu', { quoted: freply })
-}
+let handler = async m => m.reply(`
+Bot Ini Mengunakan Sc : 
+https://github.com/MikuBotz/YUKIBOTZ
+`.trim()) // Tambah sendiri kalo mau
 handler.help = ['sc']
 handler.tags = ['info']
-handler.command = /^(sc)$/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
-handler.group = false
-handler.private = false
-
-handler.admin = false
-handler.botAdmin = false
-
-handler.fail = null
-handler.limit = 0
+handler.command = /^sc$/i
 
 module.exports = handler
- 
